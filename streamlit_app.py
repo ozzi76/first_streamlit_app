31f406bdd1bbc1a22c5e66c8d902950f0c68accb
ozzi76 <79486538+ozzi76@users.noreply.github.com>
2022-04-30 15:23:57 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,6 @@ streamlit.header('My Fruits')
 
 fruits_selected = streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index), ['Avocado','Strawberries'])
 
-if len(my_fruit_list.loc[fruits_selected]) > 0:
-  streamlit.dataframe(my_fruit_list.loc[fruits_selected])
-else:
-  streamlit.dataframe(my_fruit_list)
+
+streamlit.dataframe(my_fruit_list.loc[fruits_selected])
+
